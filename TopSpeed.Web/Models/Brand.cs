cac432e9e4cff96f8db70c175e39a98398438ea4
Bunyan33 <sam.ally33@gmail.com>
2024-04-10 15:12:49 +0530

    Add project files.

---
+++ TopSpeed.Web/Models/Brand.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TopSpeed.Web.Models
+{
+    public class Brand
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Display(Name = "Established Year")]
+        [Required]
+        public int EstablishedYear { get; set; }
+        [Display(Name = "Brand Logo")]
+        public string BrandLogo { get; set; }
+    }
+}
